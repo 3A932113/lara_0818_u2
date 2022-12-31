0d4d6d43327ee61d1023ad28622479d187ecfd8d
Yang shu mei <pbs71129@gmail.com>
2022-12-31 23:36:08 +0800

    傳遞資料至View顯示

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +23,4 @@ Route::get('/', function () {
 });*/
 
 Route::get('home', [HomeController::class, 'index']);
+Route::get('hello/{name?}', [HelloController::class, 'index'])->name('hello.index');
